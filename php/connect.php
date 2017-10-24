d5a392db294dba3c07eb6ead56169ebbaca768fc
Joseph-Ladino <drtnt25@gmail.com>
2017-10-23 20:54:12 -0400

    Added support for multiple databases

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,18 @@
 <?php
-  $dbname = "nailed-it";
-  $dbhostname = "localhost";
-  $dbusername = "root";
-  $dbpassword = '';
+  if(strpos($_SERVER['REQUEST_URI'], 'nailed-it_3.0')) {
+    $dbname = "nailed-it";
+    $dbhostname = "localhost";
+    $dbusername = "root";
+    $dbpassword = '';
+  } else {
+    $url = getenv('CLEARDB_DATABASE_URL');
+    $dbparts = parse_url($url);
+    $dbname = ltrim($dbparts['path'],'/');
+  }
+  $dbhostname = $dbparts['host'];
+  $dbusername = $dbparts['user'];
+  $dbpassword = $dbparts['pass'];
+  
   $dbc = mysqli_connect($dbhostname, $dbusername, $dbpassword, $dbname) or die('could not connect to database');
   mysqli_set_charset($dbc, 'utf8');
  ?>
